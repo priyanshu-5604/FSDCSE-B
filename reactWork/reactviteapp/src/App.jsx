5d088cfd0a02f82a234be88933e495ff0395f2f9
priyanshu-5604 <priyanshu.g5604@gmail.com>
2024-11-14 13:06:25 +0530

    state hook

--- reactWork/reactviteapp/src/App.jsx
+++ reactWork/reactviteapp/src/App.jsx
@@ -1,24 +1,52 @@
 import React from 'react'
 import Student from './Student';
+import UseStudentState from './UseStudentState';
 function App() {
   const h1=<h1>Hello World</h1>
   const mystyle={
     color:'red',
     backgroundColor:'yellow'
   }
+  const studentData=[{
+        college: "ABES ENGINEERING COLLEGE" ,
+        name: " Priyanshu",
+        pic: "{<img src='https://tse1.mm.bing.net/th?id=OIP.da9lBtFNfdFQwErbLcMQwAHaFW&pid=Api&P=0&h=180' height={100} width={100}></img>",
+        branch:" CSE",
+        section:"B",
+        roll:"121"},
+        { college: "ABES",
+          name:"Rohit",
+        branch:"CS-DS",
+        section:"N",
+        roll:"12"
+        }
+  ]
   return (
-    <div style={{backgroundColor:'cyan'}}> 
-      {h1}
-      <div style={mystyle}> ABES Engineering College</div>
+    <div>
+      <h2>hello using state Hook</h2>
       <div>
-        <Student college="ABES Engineering College" 
+        <UseStudentState />
+      </div>
+    </div>
+    // <div style={{backgroundColor:'cyan'}}> 
+    //   {h1}
+    //   <div style={mystyle}> ABES Engineering College</div>
+    //   <div style={{display:'flex',margin: '50px'}}>{
+    //     studentData.map(
+    //       (ele)=>{
+    //         return <Student data={ele} />
+    //       }
+    //     )
+    //     }
+      /* <Student data={studentData}/> */
+        /* <Student college="ABES Engineering College" 
         name="Prince"
         branch="CSE"
         section="B"
         roll="12"
          />
 
-      <Student college="ABES Engineering College" 
+      <Student  
         name="Rahul"
         pic={<img src='https://tse1.mm.bing.net/th?id=OIP.da9lBtFNfdFQwErbLcMQwAHaFW&pid=Api&P=0&h=180' height={100} width={100}></img>}
         branch="CSE"
@@ -31,9 +59,9 @@ function App() {
         branch="CS-DS"
         section="N"
         roll="12"
-         />
-      </div>
-    </div>
+         /> */
+    //   </div>
+    // </div>
   )
 }
 
--- reactWork/reactviteapp/src/Student.jsx
+++ reactWork/reactviteapp/src/Student.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
-import './student.css';
-function Student(props) {
-    return (
-        <div className='icard'>
-            <table>
-                <tbody>
-                    <tr><td colSpan={2}>{props.college}</td></tr>
-                </tbody>
-            </table>
+import './student.css'
+
+function Student({data}) {
+  return (
+    <div className='icard'>
+        {/* {JSON.stringify(data)} */}
+        <table>
+            <tbody>
+                <tr><td colSpan={2}>{data.college}</td></tr>
+                <tr><td colSpan={2}>{data.pic}</td></tr>
+                <tr><td>Name</td><td>{data.name} </td></tr>
+                <tr><td>Branch</td><td>{data.branch} </td></tr>
+                <tr><td>Roll_Number</td><td>{data.roll} </td></tr>
+                <tr><td>Section</td><td>{data.section} </td></tr>
+            </tbody>
+        </table>
         </div>
-    )
+  )
 }
-
+Student.defaultProps={
+    college:"AKG Engineering College"
+}
 export default Student
-
