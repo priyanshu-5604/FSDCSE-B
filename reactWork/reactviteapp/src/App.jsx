1e09ed0f3f1867344bb072694f3189ea883db292
priyanshu-5604 <priyanshu.g5604@gmail.com>
2024-12-12 12:55:48 +0530

    Main Layout

--- reactWork/reactviteapp/src/App.jsx
+++ reactWork/reactviteapp/src/App.jsx
@@ -1,93 +1,44 @@
-import React from 'react'
-import Student from './Student';
-import UseStudentState from "./UseStudentState"
-import Imagemanipulation from './Imagemanipulation';
-import MyUseEffect from './MyUseEffect';
-import UseFetchAPI from './UseFetchAPI';
-import Login from './Login'
-import Registration from './Registration' 
+import React, { useState } from 'react'
+// import Student from './Student';
+// import UseStudentState from './UseStudentState';
+// import Imagemanipulation from './Imagemanipulation';
+// import MyUseEffect from '../MyUseEffect';
+
+import Login from './Login';
+import Registration from './Registration';
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import MainLayout from './MainLayout';
+import Dashboard from './Dashboard';
 
 function App() {
-  const h1=<h1>Hello World</h1>;
-  const mystyle={
-  color:'red',
-  backgroundColor:'yellow'
- }
-//  const studentdata =[{
-//   college:"ABES Engineering College",
-//   name:"Kuldeep",
-//   pic:<img src='https://commondatastorage.googleapis.com/codeskulptor-demos/riceracer_assets/img/srixner.png' height={100} width={100}/>,
-//   branch:"CSE",
-//   roll:"230023947937",
-//   section :"B"
-//  },
-//  {
-//   college:"ABES Engineering College",
-//   name:"Deepak",
-//   pic:<img src='https://commondatastorage.googleapis.com/codeskulptor-demos/riceracer_assets/img/srixner.png' height={100} width={100}/>,
-//   branch:"CSE",
-//   roll:"230023947937",
-//   section :"B"
-//  },
-//  {college:"ABES Engineering College",
-//  name:"Amit",
-//  pic:<img src='https://commondatastorage.googleapis.com/codeskulptor-demos/riceracer_assets/img/srixner.png' height={100} width={100}/>,
-//  branch:"CSE",
-//  roll:"30023947937",
-//  section :"B"}]
-  return (
-//     <div style={{backgroundColor:'cyan'}}>
-//       {h1}
-    
-//     <div style={mystyle}>
-//       ABES Engineering College
-//     </div>
-//     <div  style={{display:'flex'}}>
-//       {/* <Student data ={studentdata[1]}/> */
-//       studentdata.map(
-//         (ele)=>{
-//           return <Student data={ele}/>
-//         }
-//       )
-//       }
+
+  const[rdata,setrdata]=useState();
+  
  
-//       {/* <Student college="ABES Engineering College" 
-//       name="Rahul"
-//       branch="CSE"
-//       roll="2328392"
-//       section="A"
-      
-//       />
 
-// <Student college="ABES Engineering College" 
-//       name="Ankit"
-//       pic={<img src='https://commondatastorage.googleapis.com/codeskulptor-demos/riceracer_assets/img/srixner.png' height={100} width={100}/>}
-//       branch="CSE"
-//       roll="787878"
-//       section="A"
-      
-//       />
 
-//       <Student college="ABES Engineering College" 
-//       name="Amit Tomer"
-//       branch="CSE"
-//       roll="6754890"
-//       section="A"
-      
-//       /> */}
-//     </div>
-//     </div>
-  <div>
-    <h2>Hello using stateHook</h2>
+ 
+  return (
     <div>
-      {/* <UseStudentState/> */}
-      {/* <Imagemanipulation/> */}
-      {/* <MyUseEffect/> */}
-      {/* <UseFetchAPI/> */}
-      <Login/>
-      <Registration/>
-    </div>
+      {JSON.stringify(rdata)}
+  <BrowserRouter>
+  <Routes>
+ <Route path='/' element={<MainLayout />} >   
+ <Route path='/login' element={ <Login regdata={rdata} />} />
+<Route path='/registration' element={<Registration regData={setrdata} />} />
+</Route>
+
+<Route path='/dashboard' element={<Dashboard/>} />
+
+  </Routes>
+  </BrowserRouter>
+
+
+      
+ 
+  
   </div>
+      
   )
 }
 
